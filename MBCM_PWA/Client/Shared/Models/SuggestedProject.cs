954397aff8f035486b5608c501a07ed2d0e1fc15
Michael K <michael.kriel@hotmail.com>
2023-11-01 20:31:45 +0200

    Suggestions Page Basically Done.

---
+++ MBCM_PWA/Client/Shared/Models/SuggestedProject.cs
@@ -0,0 +1,13 @@
+﻿namespace MBCM_PWA.Client.Shared.Models
+{
+    public class SuggestedProject
+    {
+        public int Id { get; set; }
+
+        // Reference the user who recommended the project
+        public User Recommender { get; set; } 
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Upvotes { get; set; }
+    }
+}
