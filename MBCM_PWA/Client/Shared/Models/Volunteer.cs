79334ddecf4c3747ec052e7a977e1a09b57aeea0
Mikail Van Huyssteen <102914505+ST10085360@users.noreply.github.com>
2023-11-01 19:52:46 +0200

    Added admin pages for users and projects

---
+++ MBCM_PWA/Client/Shared/Models/Volunteer.cs
@@ -0,0 +1,10 @@
+﻿namespace MBCM_PWA.Client.Shared.Models
+{
+    public class Volunteer
+    {
+        public String FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
