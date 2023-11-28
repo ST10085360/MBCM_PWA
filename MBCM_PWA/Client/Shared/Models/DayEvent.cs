2bd4a37a66504087db24821c54f1237122a2a5f7
KokoNojoko <koko.nojoko1@gmail.com>
2023-11-28 19:08:13 +0000

    removed calendar menu item

--- MBCM_PWA/Client/Shared/Models/DayEvent.cs
+++ MBCM_PWA/Client/Shared/Models/DayEvent.cs
@@ -3,7 +3,7 @@ namespace MBCM_PWA.Client.Shared.Models
 {
     public class DayEvent
     {
-        public DateTime StartDate { get; set; } = new DateTime(1900, 1, 1);
+        public DateTime StartDate { get; set; } = new DateTime(2000, 1, 1);
         public TimeOnly Time { get; set; }
         public string Note { get; set; }
         public string DateValue { get; set; }
